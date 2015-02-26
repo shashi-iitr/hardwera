ac81af83a30ccf2010d2ce4f8f111e3d81355079
shashi kumar <shashi.kumar@hotcocoasoftware.com>
2015-02-26 23:17:43 +0530

    angular 1.3+ is used, module used without routing

--- app/app.js
+++ app/app.js
@@ -1,1 +1,3 @@
-angular.module('adminApp',[]);
+(function () {
+	angular.module('adminApp',[]);
+}());
--- app/controllers/categoriesController.js
+++ app/controllers/categoriesController.js
@@ -1,11 +1,21 @@
-function CategoriesController($scope){
-	$scope.sortBy = 'user';
-	$scope.reverse = false;
 
-	$scope.categories= [{id:'123', user:'shashi', date:'2014-07-12', status: 'approved', reason: 'lkscla'},{id:'456', user:'aseem', date:'2013-01-02', status: 'failed', reason: 'bjhcjsbcj'},{id:'789', user:'sajal', date:'2016-03-01', status: 'pending', reason: 'kjahsijkanm'},{id:'143', user:'maximus', date:'2002-01-08', status: 'corrupted', reason: 'dskjcbka,nm'},{id:'233', user:'munshi', date:'2004-03-10', status: 'chomu', reason: 'lkajsiodlksjh'}];
+(function() {
+	var CategoriesController = function($scope) {
+		$scope.sortBy = 'user';
+		$scope.reverse = false;
 
-	$scope.doSort = function(propName) {
-		$scope.sortBy = propName;
-		$scope.reverse = !$scope.reverse;
+		$scope.categories= [{id:'123', user:'shashi', date:'2014-07-12', status: 'approved', reason: 'lkscla'},{id:'456', user:'aseem', date:'2013-01-02', status: 'failed', reason: 'bjhcjsbcj'},{id:'789', user:'sajal', date:'2016-03-01', status: 'pending', reason: 'kjahsijkanm'},{id:'143', user:'maximus', date:'2002-01-08', status: 'corrupted', reason: 'dskjcbka,nm'},{id:'233', user:'munshi', date:'2004-03-10', status: 'chomu', reason: 'lkajsiodlksjh'}];
+
+		$scope.doSort = function(propName) {
+			$scope.sortBy = propName;
+			$scope.reverse = !$scope.reverse;
+		};
+
 	};
-}
+
+	CategoriesController.$inject = ['$scope'];
+
+	angular.module('adminApp')
+	.controller('CategoriesController', CategoriesController);
+
+}());
