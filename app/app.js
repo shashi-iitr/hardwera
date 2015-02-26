63fb37b761b9004b590734b50002fffa398e28f0
shashi kumar <shashi.kumar@hotcocoasoftware.com>
2015-02-26 22:31:52 +0530

    module added, in its initials

--- app/app.js
+++ app/app.js
@@ -1,14 +1,1 @@
-(function() {
-    
-    var app = angular.module('adminApp', ['ngRoute']);
-    
-    app.config(function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                controller: 'DashboardController',
-                templateUrl: 'app/views/dashboard.html'
-            })
-            .otherwise( { redirectTo: '/' } );
-    });
-    
-}());
+angular.module('adminApp',[]);
