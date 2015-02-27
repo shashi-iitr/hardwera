4e0565687e7ca9a60cfedfbe33998f86ca0efb25
shashi kumar <shashi.kumar@hotcocoasoftware.com>
2015-02-27 14:50:45 +0530

    fixing.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,16 @@
 (function () {
-	var app = angular.module('adminApp', []);
+	var app = angular.module('adminApp', ['ngRoute']);
 
-	// app.config(function($routeProvider) {
-	// 	$routeProvider
-	// 		.when('/', {
-	// 			controller: 'DashboardController',
-	// 			templateUrl: 'app/views/dashboard.html'
-	// 		})
-	// 		// .when('/category', {
-	// 		// 	controller: 'CategoriesController',
-	// 		// 	templateUrl: 'app/views/categories.html'
-	// 		// })
-	// 		.otherwise( {redirectTo: '/'});
-	// });
+	app.config(function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				controller: 'DashboardController',
+				templateUrl: 'app/views/dashboard.html'
+			})
+			.when('/category', {
+				controller: 'CategoriesController',
+				templateUrl: 'app/views/categories.html'
+			})
+			.otherwise( {redirectTo: '/'});
+	});
 }());
